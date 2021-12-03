02553e71f4140c743cda22f5b49233ee235afb0c
tmekaumput <kampanart_m@yahoo.com>
2021-12-03 13:02:10 +1100

    update format with tf fmt

--- variables.tf
+++ variables.tf
@@ -1,20 +1,20 @@
 variable "location" {
-    type = string
-    description = "Azure resources location"
+  type        = string
+  description = "Azure resources location"
 }
 
 variable "friendly_prefix" {
-    type = string
-    description = "Friendly prefix prepending to the resource names"
+  type        = string
+  description = "Friendly prefix prepending to the resource names"
 }
 
 variable "admin_permitted_object_ids" {
-    type = list(string)
-    description = "List of additional Object Ids to be granted with all permissions, the object Id of the current configuration is always added."
+  type        = list(string)
+  description = "List of additional Object Ids to be granted with all permissions, the object Id of the current configuration is always added."
 }
 
 variable "read_permitted_object_ids" {
-    type = list(string)
-    default = []
-    description = "List of additional Object Ids to be granted with read-only permissions."
+  type        = list(string)
+  default     = []
+  description = "List of additional Object Ids to be granted with read-only permissions."
 }
